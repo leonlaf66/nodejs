4b9dce7b8c77d790146480fd18acc4b9c4628350
Leon <leonlaf66@gmail.com>
2025-11-19 11:26:51 -0500

    auto commit

--- sample-app/server.js
+++ sample-app/server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the ECS!!!\n');
+  res.send('Hello World!!!\n');
 });
 
 app.get('/health', (req, res) => {
